7f4991d2e6a60856c05d20ace42c9e91242912fc
Yen Lin <e19970722@gmail.com>
2023-04-04 16:57:06 +0800

    Initial Commit

---
+++ 20230404_VOCTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20230404_VOCTest
+//
+//  Created by Yen Lin on 2023/4/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
